06b9ce24c605247b8cc2788cfa68b31a6a6c314f
Simon Gausmann <Simon.Gausmann@live.de>
2015-09-21 21:20:39 +0200

    setting port

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/api/remove', function (req, res, next) {
     next();
 });
 app.use('/', express.static('frontend/dist'));
-var server = app.listen(3000, function () {
+var server = app.listen((process.env.PORT || 5000), function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
--- index.ts
+++ index.ts
@@ -60,7 +60,7 @@ app.use('/api/remove', function(req, res, next) {
 });
 app.use('/', express.static('frontend/dist'));
 
-var server = app.listen(3000, function() {
+var server = app.listen((process.env.PORT || 5000), function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
